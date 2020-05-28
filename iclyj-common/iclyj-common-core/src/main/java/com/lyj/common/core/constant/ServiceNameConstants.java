fc64557aad27af4dd1af4b6c474ea01022ce378d
e <298692628@qq.com>
2020-05-28 11:41:01 +0800

    搭建核心模块

--- iclyj-common/iclyj-common-core/src/main/java/com/lyj/common/core/constant/ServiceNameConstants.java
+++ iclyj-common/iclyj-common-core/src/main/java/com/lyj/common/core/constant/ServiceNameConstants.java
@@ -10,10 +10,10 @@ public interface ServiceNameConstants
     /**
      * 认证服务的serviceid
      */
-    public static final String AUTH_SERVICE = "ruoyi-auth";
+    public static final String AUTH_SERVICE = "iclyj-auth";
 
     /**
      * 系统模块的serviceid
      */
-    public static final String SYSTEM_SERVICE = "ruoyi-system";
+    public static final String SYSTEM_SERVICE = "iclyj-system";
 }
