7a6dbe79428cc0ccc78fe346657b3d2bd532a61b
e <298692628@qq.com>
2020-05-29 18:04:45 +0800

    添加网关

--- iclyj-gateway/src/main/java/com/lyj/gateway/LyjGatewayApplication.java
+++ iclyj-gateway/src/main/java/com/lyj/gateway/LyjGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.lyj.gateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
@@ -10,4 +11,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })  //不加载数据源
 public class LyjGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyjGatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  网关模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" );
+    }
+
 }
