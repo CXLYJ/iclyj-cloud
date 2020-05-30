8e874a96cee0dcc3d6ba231bec0aa169a7ff5661
e <298692628@qq.com>
2020-05-30 11:21:03 +0800

    添加swagger系统模块

---
+++ iclyj-common/iclyj-common-swagger/src/main/java/com/lyj/common/swagger/annotation/EnableCustomSwagger2.java
@@ -0,0 +1,16 @@
+package com.lyj.common.swagger.annotation;
+
+import com.lyj.common.swagger.config.SwaggerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({ SwaggerAutoConfiguration.class })
+public @interface EnableCustomSwagger2
+{
+
+}
